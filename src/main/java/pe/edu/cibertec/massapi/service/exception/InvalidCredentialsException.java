b3108aaca46d3350b58131c1b24a768aa73dd648
PatrickMontes <alexandermontes30p@hotmail.com>
2024-10-08 17:17:14 -0500

    Exceptions terminado.

---
+++ src/main/java/pe/edu/cibertec/massapi/service/exception/InvalidCredentialsException.java
@@ -0,0 +1,9 @@
+package pe.edu.cibertec.massapi.service.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+
+}
