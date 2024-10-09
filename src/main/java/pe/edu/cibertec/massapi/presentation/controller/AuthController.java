b4d56373ac693c1b2c3122897e8c922853d53982
PatrickMontes <alexandermontes30p@hotmail.com>
2024-10-08 20:53:40 -0500

    AuthController hecho.

--- src/main/java/pe/edu/cibertec/massapi/presentation/controller/AuthController.java
+++ src/main/java/pe/edu/cibertec/massapi/presentation/controller/AuthController.java
@@ -1,4 +1,32 @@
 package pe.edu.cibertec.massapi.presentation.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.cibertec.massapi.presentation.dto.LoginRequest;
+import pe.edu.cibertec.massapi.presentation.dto.Respuesta;
+import pe.edu.cibertec.massapi.presentation.dto.UsuarioDTO;
+import pe.edu.cibertec.massapi.service.implementation.UsuarioService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/autenticacion")
 public class AuthController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping("/registrar")
+    private ResponseEntity<Respuesta> registerUser(@RequestBody UsuarioDTO usuarioDTO) {
+        return ResponseEntity.ok(this.usuarioService.registrarUsuario(usuarioDTO));
+    }
+
+
+    @PostMapping("/login")
+    private ResponseEntity<Respuesta> loginUser(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(this.usuarioService.loginUsuario(loginRequest));
+    }
+
 }
