4a9e8d125e3d4b46450686dd0b00fa60ccdf7ff1
PatrickMontes <alexandermontes30p@hotmail.com>
2024-10-08 19:30:15 -0500

    CorsConfig y SecurityConfig listos.

--- src/main/java/pe/edu/cibertec/massapi/security/config/CorsConfig.java
+++ src/main/java/pe/edu/cibertec/massapi/security/config/CorsConfig.java
@@ -1,4 +1,25 @@
 package pe.edu.cibertec.massapi.security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*");
+            }
+
+        };
+    }
+
 }
